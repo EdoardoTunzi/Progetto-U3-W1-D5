ac9b46d9388bbdd0bd445dfa49755e00485f24a1
Edoardo <edfl.tunzi@gmail.com>
2024-11-29 16:34:41 +0100

    test for slider component

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,6 +1,8 @@
 import { Component } from "react";
 import MovieGallery from "./MoviesGallery";
-
+import SliderGallery from "./SliderGallery";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 class Main extends Component {
   render() {
     return (
@@ -8,6 +10,7 @@ class Main extends Component {
         <MovieGallery searchQuery="star wars" name="Star Wars Collection" />
         <MovieGallery searchQuery="harry potter" name="Harry Potter Collection" />
         <MovieGallery searchQuery="lord of the rings" name="Lord of the Rings Collection" />
+        <SliderGallery />
       </div>
     );
   }
